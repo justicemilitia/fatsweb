f8d963d5169e809ea06a80ad6e89490f54d70134
edaekiz <edaekiz@github.com>
2019-02-27 18:21:00 +0300

    .

--- src/app/models/CheckOutReason.ts
+++ src/app/models/CheckOutReason.ts
@@ -1,4 +1,19 @@
-export class CheckOutReason{
+import { IData } from '../extends/TreeGridTable/models/interfaces/IData';
+
+export class CheckOutReason implements IData{
+    getParentId(): number {
+        throw new Error("Method not implemented.");
+    }
+    getChildren(): IData[] {
+        throw new Error("Method not implemented.");
+    }
+    getId(): number {
+        throw new Error("Method not implemented.");
+    }
+    childIndex: number;
+    isExtended: boolean;
+    isChecked: boolean;
+    isVisible: boolean;
     CheckOutReasonId: number;
     Name: string;
     Description: string;
--- src/app/models/ExpenseCenter.ts
+++ src/app/models/ExpenseCenter.ts
@@ -1,4 +1,19 @@
-export class ExpenseCenter{
+import { IData } from '../extends/TreeGridTable/models/interfaces/IData';
+
+export class ExpenseCenter implements IData{
+    getParentId(): number {
+        throw new Error("Method not implemented.");
+    }
+    getChildren(): IData[] {
+        throw new Error("Method not implemented.");
+    }
+    getId(): number {
+        throw new Error("Method not implemented.");
+    }
+    childIndex: number;
+    isExtended: boolean;
+    isChecked: boolean;
+    isVisible: boolean;
     ExpenseCenterId: number;
     FirmId:number;
     Name:string;
