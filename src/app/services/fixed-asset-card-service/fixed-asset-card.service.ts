acaa16d583c6a9f360b2432e2cd312c6f0dcdd8c
edaekiz <edaekiz@github.com>
2019-03-01 18:05:02 +0300

    .

--- src/app/services/fixed-asset-card-service/fixed-asset-card.service.ts
+++ src/app/services/fixed-asset-card-service/fixed-asset-card.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import {
-  HttpClient,
-} from "@angular/common/http";
-
-import { GET_HEADERS,SERVICE_URL,GET_FIXEDASSETCARD_LIST,INSERT_FIXEDASSETCARD } from "../../declarations/service-values";
+  GET_HEADERS,
+  SERVICE_URL,
+  GET_FIXEDASSETCARD_LIST,
+  INSERT_FIXEDASSETCARD,
+  UPDATE_FIXEDASSETCARD,
+  GET_FIXEDASSETCARD_BY_ID
+} from "../../declarations/service-values";
 import { AuthenticationService } from "../authenticationService/authentication.service";
-import { Response } from 'src/app/models/Response';
-import { Router } from "@angular/router";
-
+import { Response } from "src/app/models/Response";
 import { Department } from "../../models/Department";
 import { FixedAssetCard } from "../../models/FixedAssetCard";
+import { ErrorService } from "src/app/services/error-service/error.service";
 
 @Injectable({
   providedIn: "root"
@@ -17,48 +20,99 @@ import { FixedAssetCard } from "../../models/FixedAssetCard";
 export class FixedAssetCardService {
   constructor(
     private httpClient: HttpClient,
-    private router: Router,
-    private aService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private errorService: ErrorService
   ) {}
 
-  GetFixedAssetCard(callback,failed) {
+  GetFixedAssetCard(success, failed) {
     this.httpClient
       .get(SERVICE_URL + GET_FIXEDASSETCARD_LIST, {
-        headers: GET_HEADERS(this.aService.getToken())
+        headers: GET_HEADERS(this.authenticationService.getToken())
       })
-      .subscribe(result => {
-        
-        let response: Response = <Response>result;
-        let fixedAssetCards: FixedAssetCard[] = [];
-        
-        (<FixedAssetCard[]>response.ResultObject).forEach((e) => {
-            let fac: FixedAssetCard = new FixedAssetCard();
-            Object.assign(fac, e);
-            fixedAssetCards.push(fac);
-        });
+      .subscribe(
+        result => {
+          let response: Response = <Response>result;
+          if (response.ResultStatus == true) {
+            let fixedAssetCards: FixedAssetCard[] = [];
+            (<FixedAssetCard[]>response.ResultObject).forEach(e => {
+              let fac: FixedAssetCard = new FixedAssetCard();
+              Object.assign(fac, e);
+              fixedAssetCards.push(fac);
+            });
+            success(fixedAssetCards, response.LanguageKeyword);
+          } else {
+            failed(
+              this.errorService.getAnErrorResponse(response.LanguageKeyword)
+            );
+          }
+        },
+        (error: HttpErrorResponse) => {
+          failed(error);
+        }
+      );
+  }
 
-        callback(fixedAssetCards);
-        
-      },
+  InsertFixedAssetCard(fixedAssetCard: FixedAssetCard, success, failed) {
+    debugger;
+    this.httpClient
+      .post(SERVICE_URL + INSERT_FIXEDASSETCARD, fixedAssetCard, {
+        headers: GET_HEADERS(this.authenticationService.getToken())
+      })
+      .subscribe(
+        result => {
+          let response: Response = <Response>result;
+          if (response.ResultStatus == true) {
+            let insertedFixedAssetCard = new FixedAssetCard();
+            Object.assign(insertedFixedAssetCard, response.LanguageKeyword);
+            success(insertedFixedAssetCard, response.LanguageKeyword);
+          } else {
+            failed(
+              this.errorService.getAnErrorResponse(response.LanguageKeyword)
+            );
+          }
+        },
         error => {
           failed(error);
         }
       );
   }
 
-  InsertFixedAssetCard(fixedAssetCard: FixedAssetCard) {
-    debugger;
-    this.httpClient
-      .post(SERVICE_URL + INSERT_FIXEDASSETCARD, fixedAssetCard, {
-        headers: GET_HEADERS(this.aService.getToken())
-      })
-      .subscribe(
-        data => {
-          console.log(data);
-        },
-        error => {
-          console.log(error);
+  UpdateFixedAssetCard(fixedAssetCard: FixedAssetCard, success, failed) {
+    this.httpClient.put(SERVICE_URL + UPDATE_FIXEDASSETCARD, fixedAssetCard, {
+      headers: GET_HEADERS(this.authenticationService.getToken())
+    }).subscribe(
+      result => {
+        let response: Response = <Response>result;
+        if (response.ResultStatus == true) {
+          let _updatedFixedAssetCard: FixedAssetCard = new FixedAssetCard();
+          Object.assign(_updatedFixedAssetCard, fixedAssetCard);
+          success(_updatedFixedAssetCard, response.LanguageKeyword);
+        } else {
+          failed(this.errorService.getAnErrorResponse(response.LanguageKeyword));
         }
-      );
+      },
+      error => {
+        failed(error);
+      }
+    );
+  }
+
+  GetFixedAssetCardById(fixedAssetCardId: number, success, failed) {
+    this.httpClient
+      .get(SERVICE_URL + GET_FIXEDASSETCARD_BY_ID + "/" + fixedAssetCardId, {
+        headers: GET_HEADERS(this.authenticationService.getToken())
+      })
+      .subscribe(result => {
+        let response: Response = <Response>result;
+        if (response.ResultStatus == true) {
+          let fixedAssetCard: FixedAssetCard = new FixedAssetCard();
+          Object.assign(fixedAssetCard, response.ResultObject);
+          success(fixedAssetCard, response.LanguageKeyword);
+        } else {
+          failed(this.errorService.getAnErrorResponse(response.LanguageKeyword));
+        }
+      }, error => {
+        failed(error);
+      });
   }
 }
