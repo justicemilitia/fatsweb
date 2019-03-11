1296c4c0622b146fb32532f0cf8179d940958f7c
SenaSelinSariyilmaz <Sena.SARIYILMAZ@trinoks.com>
2019-03-11 16:39:45 +0300

    fixed asset status

--- src/app/models/FixedAssetStatus.ts
+++ src/app/models/FixedAssetStatus.ts
@@ -1,20 +1,24 @@
+import { FixedAsset } from './FixedAsset';
 import { IData } from '../extends/TreeGridTable/models/interfaces/IData';
 
-export class CheckOutReason implements IData{
+export class FixedAssetStatus implements IData{
     getParentId(): number {
       return null;
     }
     getChildren(): IData[] {
-     return [];
+        return [];
     }
     getId(): number {
-       return this.CheckOutReasonId;
+        return this.FixedAssetStatusId;
     }
     childIndex: number;
     isExtended: boolean;
     isChecked: boolean;
     isVisible: boolean;
-    CheckOutReasonId: number;
-    Name: string;
-    Description: string;
+    
+    FixedAssetStatuCode:number;
+    FixedAssetStatusId:number;
+    Name:string;
+    Color:string;
+    FixedAssets:FixedAsset;
 }
--- src/app/services/check-out-reason-service/check-out-reason.service.ts
+++ src/app/services/check-out-reason-service/check-out-reason.service.ts
@@ -4,15 +4,14 @@ import { NgForm } from "@angular/forms";
 import {
   GET_HEADERS,
   SERVICE_URL,
-  INSERT_CHECKOUTREASON,
   GET_CHECKOUTREASON_LIST,
-  UPDATE_CHECKOUTREASON,
-  GET_CHECKOUTREASON_BY_ID
+  GET_FIXEDASSETSTATUS_LIST
 } from "../../declarations/service-values";
 import { CheckOutReason } from "src/app/models/CheckOutReason";
 import { AuthenticationService } from "../authenticationService/authentication.service";
 import { Response } from "src/app/models/Response";
 import { getAnErrorResponse } from 'src/app/declarations/extends';
+import { FixedAssetStatus } from 'src/app/models/FixedAssetStatus';
 @Injectable({
   providedIn: "root"
 })
@@ -37,6 +36,28 @@ export class CheckOutReasonService {
               reasons.push(reason);
             });
             success(reasons, response.LanguageKeyword);
+          } else {
+            failed(getAnErrorResponse(response.LanguageKeyword));
+          }
+        }, (error: HttpErrorResponse) => {
+          failed(error);
+        });
+  }
+
+  GetFixedAssetStatus(success, failed) {
+    this.httpClient
+      .get(SERVICE_URL + GET_FIXEDASSETSTATUS_LIST, { headers: GET_HEADERS(this.aService.getToken()) })
+      .subscribe(
+        result => {
+          let response: Response = <Response>result;
+          if (response.ResultStatus == true) {
+            let status: FixedAssetStatus[] = [];
+            (<FixedAssetStatus[]>response.ResultObject).forEach(e => {
+              let statu: FixedAssetStatus = new FixedAssetStatus();
+              Object.assign(statu, e);
+              status.push(statu);
+            });
+            success(status, response.LanguageKeyword);
           } else {
             failed(getAnErrorResponse(response.LanguageKeyword));
           }
