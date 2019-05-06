811344f02e21aa084c21806ea47be5506fdba7a7
edaekiz <edaekiz@github.com>
2019-05-06 16:19:47 +0300

    .

--- src/app/declarations/service-values.ts
+++ src/app/declarations/service-values.ts
@@ -1,15 +1,9 @@
 import { HttpHeaders } from "@angular/common/http";
 
 
-<<<<<<< HEAD
 export const SERVICE_URL = "http://localhost:5000/api/";
 //export const SERVICE_URL = "http://localhost:11889/api/";
 // export const SERVICE_URL = "http://dev.fatsapi.com/api/";
-=======
-//export const SERVICE_URL = "http://localhost:5000/api/";
-export const SERVICE_URL = "http://localhost:11889/api/";
-//export const SERVICE_URL = "http://dev.fatsapi.com/api/";
->>>>>>> 960c8b53a435efecfaea9bc7cadf5229b35d5185
 export const LOGIN = "Auth/token";
 
 export const FORGET_PASSWORD = "Auth/forgotpassword"
